24095bda7240c761f0eaad7306634e31efb264bf
funTechCn <39047703+funTechCn@users.noreply.github.com>
2023-01-30 09:05:39 +0800

    增加过滤配置

--- config/config.go
+++ config/config.go
@@ -11,6 +11,13 @@ import (
 type Configuration struct {
 	// gtp apikey
 	ApiKey string `json:"api_key"`
+
+	//允许回复群列表
+	AutoReplyGroups string `json:"auto_reply_groups"`
+
+	//允许回复好友列表
+	AutoReplyFriends string `json:"auto_reply_friends"`
+	
 	// 自动通过好友
 	AutoPass bool `json:"auto_pass"`
 }
@@ -39,8 +46,17 @@ func LoadConfig() *Configuration {
 		// 如果环境变量有配置，读取环境变量
 		ApiKey := os.Getenv("ApiKey")
 		AutoPass := os.Getenv("AutoPass")
+		AutoReplyGroups := os.Getenv("AutoReplyGroups")
+		AutoReplyFriends := os.Getenv("AutoReplyFriends")
+
 		if ApiKey != "" {
 			config.ApiKey = ApiKey
+		}
+		if AutoReplyGroups != "" {
+			config.AutoReplyGroups = AutoReplyGroups
+		}
+		if AutoReplyFriends != "" {
+			config.AutoReplyFriends = AutoReplyFriends
 		}
 		if AutoPass == "true" {
 			config.AutoPass = true
